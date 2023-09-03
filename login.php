8b32797cc982f322afcdb76a7afc5f1d0c6f0ed1
ACRONYM <shiba19981216@gmail.com>
2023-09-03 16:53:58 +0800

    check token 導向首頁

--- login.php
+++ login.php
@@ -1,3 +1,8 @@
+<?php session_start();
+if (isset($_COOKIE['token'])) {
+    header("location: main.php");
+} else {
+    }?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,6 +11,11 @@
     <title>Login</title>
 </head>
 <body>
-    
+    <form action="loginCheck.php" method="post">
+        <input placeholder="Email" name="email" id="email"><br>
+        <input placeholder="password" name="pwd" id="pwd"><br>
+        <input type="submit" value="login">
+        <input type="submit" value="Sign Up">
+        </form>
 </body>
 </html>
